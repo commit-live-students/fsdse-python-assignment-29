a472847760a076d5deaddbecfd2b2612a517ec8a
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-14 20:37:26 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(dic):
     "Enter Code here"
+    maxKey = max(dic, key=dic.get)
+    minKey = min(dic, key=dic.get)
+    return (dic[maxKey],dic[minKey])
+
+solution({'a': 1, 'b': 2, 'c': 3})
