8787203f68733f2aa44fcac129ba0bf257b9e704
nixen420 <nikhil.singh4008@gmail.com>
2017-06-18 16:09:29 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
-def solution(dic):
-    "Enter Code here"
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+list1=[]
+def solution(my_dict):
+    key_min = min(my_dict.keys(), key=(lambda k: my_dict[k]))
+    key_max = max(my_dict.keys(), key=(lambda k: my_dict[k]))
+
+    list1=(my_dict[key_max],my_dict[key_min])
+    return tuple(list1)
+solution(my_dict)
