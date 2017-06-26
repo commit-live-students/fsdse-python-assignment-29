d54a18d3904a0e52774223bd72981727b2ebc7f4
varunpanicker <varunpanicker@users.noreply.github.com>
2017-06-26 18:34:38 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(dic):
-    "Enter Code here"
+    for key in dic:
+        key_max = max(dic.keys(), key=(lambda k: dic[k]))
+        key_min = min(dic.keys(), key=(lambda k: dic[k]))
+
+        return(dic[key_max],dic[key_min])
+#        return(dic[key_min])
+
+print solution({'x':500, 'y':5874, 'z': 560})
